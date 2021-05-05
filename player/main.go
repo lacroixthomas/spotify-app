f45193ff3de8e26acafcfe3e669bf36757ff0074
Thomas Lacroix <thomaslacroix@Thomass-MacBook-Pro.local>
2021-05-05 16:18:07 +0100

    [Server] Add base of player microservice

---
+++ player/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2"
+)
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	bearer := r.Header.Get("Authorization")
+	token := new(oauth2.Token)
+	token.AccessToken = bearer
+	client := spotify.Authenticator{}.NewClient(token)
+	player, err := client.PlayerCurrentlyPlaying()
+	if err != nil {
+		return
+	}
+
+	json.NewEncoder(w).Encode(player)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/player", userHandler).Methods("GET")
+
+	handler := cors.Default().Handler(r)
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
